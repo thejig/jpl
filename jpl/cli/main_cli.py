7e5747f44e5bb25f63fe07c11055d5c3ebc7a009
leonkozlowski <leonkozlowski@gmail.com>
2020-02-18 21:58:21 -0500

    is_flag added

--- jpl/cli/main_cli.py
+++ jpl/cli/main_cli.py
@@ -13,25 +13,25 @@ MARK_TO_COLOR = {"PASSED": "green", "WARNING": "yellow", "FAILED": "red"}
     "-s",
     "--skip",
     required=False,
-    default="true",
+    is_flag=True,
+    default=True,
     help="Skip `PASSED` rules in jpl report",
 )
 @click.option("-p", "--playbook", required=True, help="Filepath to Jiggy Playbook")
 def lint(verbose, skip, playbook):
-    """
-    Click CLI entrypoint to run JiggyPlaybookLint.
+    """Click CLI entrypoint to run JiggyPlaybookLint.
 
     CLI Args:
-        -v --verbose: Run `jpl` with verbosity.
-        -s --skip: Skip "PASSED" rules in JiggyPlaybookLint Report.
-        -p --playbook: Location of JiggyPlaybook to lint.
 
-    Args:
-        verbose: (count) - flag to denote response with verbosity
-        skip: (str) - Skip "PASSED" rules in JiggyPlaybookLint Report.
-        playbook: (str) - Location of JiggyPlaybook to lint.
+    -v --verbose: Run `jpl` with verbosity.
+
+    -s --skip: Skip "PASSED" rules in JiggyPlaybookLint Report.
+
+    -p --playbook: Location of JiggyPlaybook to lint.
+
 
     Returns:
+
         click.echo - `with style`
     """
     click.echo(
@@ -43,9 +43,7 @@ def lint(verbose, skip, playbook):
 \/_____/iggy  \/_/laybook \/_____/inter            
         """
     )
-    linted = jpl.JiggyPlaybookLint(
-        path=playbook, skip=bool(skip.lower() in ["true", True])
-    ).run()
+    linted = jpl.JiggyPlaybookLint(path=playbook, skip=skip).run()
 
     generate_jpl_report(linted=linted, verbose=verbose)
 
@@ -68,12 +66,22 @@ def generate_jpl_report(linted: list, verbose=None):  # pragma no cover
                 rule.rule, rule.__class__.__name__, task_name
             )
 
-        if verbose:
+        if verbose == 1:
             click.echo(
                 "{:<50}{:<20} {}".format(
                     rule_meta,
                     click.style(mark, fg=MARK_TO_COLOR.get(mark)),
                     rule.message,
+                )
+            )
+        elif verbose > 1:
+            click.echo(
+                "{:<50}{:<20} {} \nPriority: `{}` - Description: `{}`\n".format(
+                    rule_meta,
+                    click.style(mark, fg=MARK_TO_COLOR.get(mark)),
+                    rule.message,
+                    rule.priority,
+                    rule.description,
                 )
             )
         else:
